fbd47e4b1f745477dac289efe5fcf10cedad1ed9
Zeno Rocha <hi@zenorocha.com>
2016-05-10 10:25:56 -0700

    Adds fake user name

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,7 @@
-/* Meme */
+/* User */
 
-var memes = [
-	'Dude, you smashed my turtle saying "I\'M MARIO BROS!"',
-	'Dude, you grabed seven oranges and yelled "I GOT THE DRAGON BALLS!"',
-	'Dude, you threw my hamster across the room and said "PIKACHU I CHOOSE YOU!"',
-	'Dude, you congratulated a potato for getting a part in Toy Story',
-	'Dude, you were hugging an old man with a beard screaming "DUMBLEDORE YOU\'RE ALIVE!"',
-	'Dude, you were cutting all my pinapples yelling "SPONGEBOB! I KNOW YOU\'RE THERE!"',
-];
-
-var random = document.querySelector('#random');
-
-random.innerHTML = memes[Math.floor(Math.random() * memes.length)];
-
-/* Time */
-
-var deviceTime = document.querySelector('.status-bar .time');
-var messageTime = document.querySelectorAll('.message .time');
-
-deviceTime.innerHTML = moment().format('h:mm');
-
-setInterval(function() {
-	deviceTime.innerHTML = moment().format('h:mm');
-}, 1000);
-
-for (var i = 0; i < messageTime.length; i++) {
-	messageTime[i].innerHTML = moment().format('h:mm A');
-}
+var myName = faker.name.firstName();
+var myColor = 'color-' + Math.floor(Math.random() * 19);
 
 /* Message */
 
@@ -39,7 +14,16 @@ function newMessage(e) {
 	var input = e.target.input;
 
 	if (input.value) {
-		var message = buildMessage(input.value);
+		var data = {
+			author: {
+				name: myName,
+				color: myColor
+			},
+			content: input.value,
+			time: moment().format('h:mm A')
+		};
+
+		var message = buildMessage(data);
 		conversation.appendChild(message);
 		animateMessage(message);
 	}
@@ -50,14 +34,15 @@ function newMessage(e) {
 	e.preventDefault();
 }
 
-function buildMessage(text) {
+function buildMessage(data) {
 	var element = document.createElement('div');
 
 	element.classList.add('message', 'sent');
 
-	element.innerHTML = text +
+	element.innerHTML = '<span class="user">' + data.author.name + '</span>' +
+		'<span class="text">' + data.content + '</span>' +
 		'<span class="metadata">' +
-			'<span class="time">' + moment().format('h:mm A') + '</span>' +
+			'<span class="time">' + data.time + '</span>' +
 			'<span class="tick tick-animation">' +
 				'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck" x="2047" y="2061"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#92a58c"/></svg>' +
 				'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg>' +
@@ -73,3 +58,13 @@ function animateMessage(message) {
 		tick.classList.remove('tick-animation');
 	}, 500);
 }
+
+/* Time */
+
+var deviceTime = document.querySelector('.status-bar .time');
+
+deviceTime.innerHTML = moment().format('h:mm');
+
+setInterval(function() {
+	deviceTime.innerHTML = moment().format('h:mm');
+}, 1000);
