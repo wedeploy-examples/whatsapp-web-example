9d2a8af821676433108dc0e0c9ed8efa33f7593a
Zeno Rocha <hi@zenorocha.com>
2016-05-09 23:01:56 -0700

    Adds random memes

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
+/* Meme */
+
+var memes = [
+	'Dude, you smashed my turtle saying "I\'M MARIO BROS!"',
+	'Dude, you grabed seven oranges and yelled "I GOT THE DRAGON BALLS!"',
+	'Dude, you threw my hamster across the room and said "PIKACHU I CHOOSE YOU!"',
+	'Dude, you congratulated a potato for getting a part in Toy Story',
+	'Dude, you were hugging an old man with a beard screaming "DUMBLEDORE YOU\'RE ALIVE!"',
+	'Dude, you were cutting all my pinapples yelling "SPONGEBOB! I KNOW YOU\'RE THERE!"',
+];
+
+var random = document.querySelector('#random');
+
+random.innerHTML = memes[Math.floor(Math.random() * memes.length)];
+
 /* Time */
 
 var deviceTime = document.querySelector('.status-bar .time');
